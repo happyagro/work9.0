9d8d95d9641811dc21c6e8e9feb900faec9ab200
Nikita Aniskovets <happyaggro@gmail.com>
2021-05-04 19:46:02 +0300

    added class PostManager for task 2

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,17 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+import ru.netology.manager.*;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] search(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset) {
+
+        return null;
+    }
+
+    public void delete(int ownerId, int postId) {
+
+    }
+}
